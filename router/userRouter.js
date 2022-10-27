06da2e71685576d5460f8015e4b26ab555674e54
Yacdany Arguedas Mora <jack20-21@hotmail.com>
2022-10-27 14:06:13 -0600

    Showing users from the database

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,7 +1,8 @@
 const express = require('express');
-const { getUsers } = require('../controllers/userController');
+const { getUsers, addUser } = require('../controllers/userController');
 const userRouter=express.Router();
 
 userRouter.get('/',getUsers);
+userRouter.post('/',addUser);
 
 module.exports = userRouter;
